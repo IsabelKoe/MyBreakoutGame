7041b09ecf3177e29b282628ef2db4724e3100d6
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-20 10:07:28 +0200

    Grabbing HTML Elements in domutils.ts file for further use. Made some class-/id-name corrections.

---
+++ src/modules/domutils.ts
@@ -0,0 +1,7 @@
+const canvas: HTMLCanvasElement | null = document.querySelector('#playfield-canvas');
+const currentScore: HTMLElement | null = document.querySelector('#current-score');
+const startBtn: HTMLButtonElement | null = document.querySelector('#start-button');
+const playerInfo: HTMLElement | null = document.querySelector('#player-info');
+
+
+export { canvas, currentScore, startBtn, playerInfo }
