6984927ac76371500618326f75d642b1437d2509
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-20 12:27:51 +0200

    Created Setter method in Brick class to be able to give bricks different difficulty levels.

--- src/modules/GameElements/Brick.ts
+++ src/modules/GameElements/Brick.ts
@@ -1,45 +1,49 @@
-
 export class Brick {
-    private _brickImg: HTMLImageElement;
+  private _brickImg: HTMLImageElement;
 
-    constructor(
-        image: HTMLImageElement,
-        private _xPosition: number,
-        private _yPosition: number,
-        private _brickWidth: number,
-        private _brickHeight: number,
-        private _brickDifficulty: number
-    ) {
-        this._brickImg = image;
-        this._xPosition = _xPosition;
-        this._yPosition = _yPosition;
-        this._brickWidth = _brickWidth;
-        this._brickHeight = _brickHeight;
-        this._brickDifficulty = _brickDifficulty;
-    }
+  constructor(
+    image: HTMLImageElement,
+    private _xPosition: number,
+    private _yPosition: number,
+    private _brickWidth: number,
+    private _brickHeight: number,
+    private _brickDifficulty: number
+  ) {
+    this._brickImg = image;
+    this._xPosition = _xPosition;
+    this._yPosition = _yPosition;
+    this._brickWidth = _brickWidth;
+    this._brickHeight = _brickHeight;
+    this._brickDifficulty = _brickDifficulty;
+  }
 
-    //Getter Methoden, um an die verschiedenen Properties von außen heranzukommen
-    getImg(): HTMLImageElement {
-        return this._brickImg;
-    }
+  //Getter Methoden, um an die verschiedenen Properties von außen heranzukommen
+  getImg(): HTMLImageElement {
+    return this._brickImg;
+  }
 
-    getXPosition(): number {
-        return this._xPosition;
-    }
+  getXPosition(): number {
+    return this._xPosition;
+  }
 
-    getYPosition(): number {
-        return this._yPosition;
-    }
+  getYPosition(): number {
+    return this._yPosition;
+  }
 
-    getWidth(): number {
-        return this._brickWidth;
-    }
+  getWidth(): number {
+    return this._brickWidth;
+  }
 
-    getHeight(): number {
-        return this._brickHeight;
-    }
+  getHeight(): number {
+    return this._brickHeight;
+  }
 
-    getDifficulty(): number {
-        return this._brickDifficulty;
-    }
+  getDifficulty(): number {
+    return this._brickDifficulty;
+  }
+
+  //Setter Methode, um den Schwierigkeitsgrad eines einzelnen Bricks zu setzen (wie oft er getroffen werden muss bis er verschwindet)
+  setBrickDifficulty(difficultyLevel: number) {
+      this._brickDifficulty = difficultyLevel;
+  }
 }
