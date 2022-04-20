2d3071d57c00ea6570e07954bf7f7d59e98b63a5
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-20 15:15:35 +0200

    Correction of imports and implementing gameLoop and startGame function in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,32 @@ import { CanvasView } from "./modules/Canvas";
 import { Brick } from "./modules/GameElements/Brick";
 import { Paddle } from "./modules/GameElements/Paddle";
 import { Ball } from "./modules/GameElements/Ball";
-import { ballImg, blueBrickImg, greenBrickImg, purpleBrickImg, redBrickImg, yellowBrickImg, paddleImg } from "./modules/domutils";  
+import { createBrickArray } from './modules/BrickArray';
+
+let gameOver = false;
+let score = 0;
+
+function gameLoop(
+  game: CanvasView,
+  bricks: Brick [],
+
+) {
+  game.clear();
+  game.drawBricks(bricks);
+
+  // AnimationFrame to create the gameLoop forever and forever
+  requestAnimationFrame(() => gameLoop(game, bricks));
+};
 
 function startGame(game: CanvasView) {
+  //reset displays
+  score = 0;
+  game.displayScore(score);
+  game.displayPlayerInfo('');
+  //Create all bricks
+  const bricks = createBrickArray();
+
+  gameLoop(game, bricks)
 
 };
 
