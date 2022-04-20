108f7be71a9d5bdd626be0749bdda946dee0853a
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-20 10:54:26 +0200

    Created Img Elements for Game Images and set src in images.ts file.

---
+++ src/images/images.ts
@@ -0,0 +1,28 @@
+import {
+  ballImg,
+  blueBrickImg,
+  greenBrickImg,
+  purpleBrickImg,
+  redBrickImg,
+  yellowBrickImg,
+  paddleImg,
+} from "../modules/domutils";
+
+//Set src to all image elements
+ballImg.src = "src/images/ball.png";
+blueBrickImg.src = "src/images/brick-blue.png";
+greenBrickImg.src = "src/images/brick-green.png";
+purpleBrickImg.src = "src/images/brick-purple.png";
+redBrickImg.src = "src/images/brick-red.png";
+yellowBrickImg.src = "src/images/brick-yellow.png";
+paddleImg.src = "src/images/paddle.png";
+
+export {
+  ballImg,
+  blueBrickImg,
+  greenBrickImg,
+  purpleBrickImg,
+  redBrickImg,
+  yellowBrickImg,
+  paddleImg,
+};
--- src/modules/domutils.ts
+++ src/modules/domutils.ts
@@ -1,7 +1,30 @@
-const canvas: HTMLCanvasElement | null = document.querySelector('#playfield-canvas');
-const currentScore: HTMLElement | null = document.querySelector('#current-score');
-const startBtn: HTMLButtonElement | null = document.querySelector('#start-button');
-const playerInfo: HTMLElement | null = document.querySelector('#player-info');
+//Grab or create HTML Elements needed for game
+const canvas: HTMLCanvasElement | null =
+  document.querySelector("#playfield-canvas");
+const currentScore: HTMLElement | null =
+  document.querySelector("#current-score");
+const startBtn: HTMLButtonElement | null =
+  document.querySelector("#start-button");
+const playerInfo: HTMLElement | null = document.querySelector("#player-info");
+const ballImg: HTMLImageElement = document.createElement("img");
+const blueBrickImg: HTMLImageElement = document.createElement("img");
+const greenBrickImg: HTMLImageElement = document.createElement("img");
+const purpleBrickImg: HTMLImageElement = document.createElement("img");
+const redBrickImg: HTMLImageElement = document.createElement("img");
+const yellowBrickImg: HTMLImageElement = document.createElement("img");
+const paddleImg: HTMLImageElement = document.createElement("img");
 
-
-export { canvas, currentScore, startBtn, playerInfo }
+//export HTML Elements
+export {
+  canvas,
+  currentScore,
+  startBtn,
+  playerInfo,
+  ballImg,
+  blueBrickImg,
+  greenBrickImg,
+  purpleBrickImg,
+  redBrickImg,
+  yellowBrickImg,
+  paddleImg,
+};
