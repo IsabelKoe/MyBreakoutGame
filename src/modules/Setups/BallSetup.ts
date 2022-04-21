597123dbba5e05fe249f4d0593f4e58d9ac7d0aa
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-21 22:42:09 +0200

    Created ballSetup to define some start values for the ball.

---
+++ src/modules/Setups/BallSetup.ts
@@ -0,0 +1,8 @@
+import { ballImg } from "../../images/Images";
+// defining size, speed and start position of ball
+const ballSpeed = 5;
+const ballSize = 20;
+const balltXStartPos = 500;
+const ballYStartPos = 400;
+
+export {ballImg, ballSpeed, ballSize, balltXStartPos, ballYStartPos}
