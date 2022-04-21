15dfac5eea93df8669387cbde2874c1c13da0b10
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-21 21:27:19 +0200

    Created Variables and Constructor for Paddle class.

--- src/modules/GameElements/Paddle.ts
+++ src/modules/GameElements/Paddle.ts
@@ -1,4 +1,25 @@
-
 export class Paddle {
+    private _paddleImg: HTMLImageElement;
+    private _movingLeft: boolean;
+    private _movingRight: boolean;
+
+    constructor(
+        image: HTMLImageElement,
+        private _xPosition: number,
+        private _yPosition: number,
+        private _paddleWidth: number,
+        private _paddleHeight: number,
+        private _paddleSpeed: number
+    ) {
+        this._paddleImg = image;
+        this._xPosition = _xPosition;
+        this._yPosition = _yPosition;
+        this._paddleWidth = _paddleWidth;
+        this._paddleHeight = _paddleHeight;
+        this._paddleSpeed = _paddleSpeed;
+        this._movingLeft = false;
+        this._movingRight = false;
+    }
+
     
 }
