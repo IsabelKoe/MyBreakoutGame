9536f852fd979f90da34aec356f8a048e8982ee4
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-21 21:56:51 +0200

    Created paddleSetup module to define some basic values for the paddle.

---
+++ src/modules/Setups/PaddleSetup.ts
@@ -0,0 +1,7 @@
+// defining width, height, startposition and speed of paddle
+const paddleWidth = 150;
+const paddleHeight = 25;
+const paddleStartX = 450;
+const paddleSpeed = 10;
+
+export { paddleWidth, paddleHeight, paddleStartX, paddleSpeed}
