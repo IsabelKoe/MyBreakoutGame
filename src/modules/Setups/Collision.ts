464e91f248ef1af7657221a5d80a727b5c50b556
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-22 18:43:40 +0200

    Implemented Collision methods to check for Ball colliding with canvas walls or paddle and bricks. Implemented logic that brick difficulty level will be reduced when hit by ball.

--- src/modules/Setups/Collision.ts
+++ src/modules/Setups/Collision.ts
@@ -2,4 +2,62 @@ import { Ball } from "../GameElements/Ball";
 import { Paddle } from "../GameElements/Paddle";
 import { Brick } from "../GameElements/Brick";
 import { CanvasView } from "../GameElements/CanvasView";
+// imports only used as types - no instances will be created
+//class to collect all collision methods in one class
+export class Collision {
+// method to check whether the ball hits another element (paddle or canvas wall)
+    checkBallColliding(ball: Ball, paddle: Paddle, game: CanvasView): void {
+        // Collision with paddle
+        if(
+            ball.getXPosition() + ball.getWidth() > paddle.getXPosition() &&
+            ball.getXPosition() < paddle.getXPosition() + paddle.getWidth() &&
+            ball.getYPosition() + ball.getHeight() === paddle.getYPosition()
+        ) {
+            ball.changeDirectionY();
+        }
+        // Collision with canvas walls
+        //right and left walls
+        if(
+            ball.getXPosition() > game.canvas.width - ball.getWidth() ||
+            ball.getXPosition() < 0
+        ) {
+            ball.changeDirectionX();
+        }
+        // top wall
+        if(ball.getYPosition() < 0) {
+            ball.changeDirectionY;
+        }
+    }
+    //check if ball hits a brick
+    checkBallCollidingBrick(ball: Ball, brick: Brick): boolean {
+        if(
+            ball.getXPosition() < brick.getXPosition() + brick.getWidth() &&
+            ball.getXPosition() + ball.getWidth() > brick.getXPosition() &&
+            ball.getYPosition() < brick.getYPosition() + brick.getHeight() &&
+            ball.getYPosition() + ball.getHeight() > brick.getYPosition()
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // if ball hits one or more bricks, we have to reduce the bricks difficulty level by one
+    // if bricks dificulty is 1, we have to delete the brick from display
+    reduceBricksOnCollision(ball: Ball, bricks: Brick[]): boolean {
+        let collision = false;
+        bricks.forEach((brick, i) => {
+            if(this.checkBallCollidingBrick(ball, brick)) {
+                ball.changeDirectionY();
 
+                if(brick.getDifficulty() === 1) {
+                    bricks.splice(i, 1);
+                } else {
+                let newDifficulty = brick.getDifficulty() -1;
+                brick.setBrickDifficulty(newDifficulty);  
+                }
+                collision = true;
+            }
+        });
+        return collision;
+    }
+}
