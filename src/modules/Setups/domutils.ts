409c1c4770d8eff39be8260162b501ccc72ce5ae
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-22 21:34:37 +0200

    Edited some styling of the application.

--- src/modules/Setups/domutils.ts
+++ src/modules/Setups/domutils.ts
@@ -1,10 +1,10 @@
-//Grab or create HTML Elements needed for game
+//Grab or create HTML Elements needed for playing the game
 const canvas: HTMLCanvasElement | null =
   document.querySelector("#playfield-canvas");
 const currentScore: HTMLObjectElement | null =
   document.querySelector("#current-score");
 const startBtn: HTMLButtonElement | null =
-  document.querySelector("#start-button");
+  document.querySelector(".playBtn");
 const playerInfo: HTMLObjectElement | null = document.querySelector("#player-info");
 const ballImg: HTMLImageElement = document.createElement("img");
 const blueBrickImg: HTMLImageElement = document.createElement("img");
@@ -13,6 +13,10 @@ const purpleBrickImg: HTMLImageElement = document.createElement("img");
 const redBrickImg: HTMLImageElement = document.createElement("img");
 const yellowBrickImg: HTMLImageElement = document.createElement("img");
 const paddleImg: HTMLImageElement = document.createElement("img");
+
+//Grab HTML Elements outside grid-gamefield
+const nameBtn: HTMLButtonElement | null = document.querySelector('.nameBtn')
+
 
 //export HTML Elements
 export {
