b1905939fa38a4e39fe1590f7db9f25a7701510f
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-22 17:31:25 +0200

    Corrected misspelling of ballYStartPos.

--- src/modules/Setups/BallSetup.ts
+++ src/modules/Setups/BallSetup.ts
@@ -2,7 +2,7 @@ import { ballImg } from "../../images/Images";
 // defining size, speed and start position of ball
 const ballSpeed = 5;
 const ballSize = 20;
-const balltXStartPos = 500;
+const ballXStartPos = 500;
 const ballYStartPos = 400;
 
-export {ballImg, ballSpeed, ballSize, balltXStartPos, ballYStartPos}
+export {ballImg, ballSpeed, ballSize, ballXStartPos, ballYStartPos}
