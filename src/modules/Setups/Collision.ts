7ead76c73cbb7db7b267f0576f463ec3b8a0a021
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-22 17:42:17 +0200

    Created a new Collision Class and renamed Canvas to CanvasView like the class.

---
+++ src/modules/Setups/Collision.ts
@@ -0,0 +1,5 @@
+import { Ball } from "../GameElements/Ball";
+import { Paddle } from "../GameElements/Paddle";
+import { Brick } from "../GameElements/Brick";
+import { CanvasView } from "../GameElements/CanvasView";
+
