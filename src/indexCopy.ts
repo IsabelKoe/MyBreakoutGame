4e329ed56ecb52f6d8ed4215b44a002326a52087
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-27 11:48:18 +0200

    Created HTML and Styling for a first startpage.

---
+++ src/indexCopy.ts
@@ -0,0 +1,7 @@
+// const btnStartGame = document.querySelector("#playBtn");
+
+// btnStartGame?.addEventListener("click", () => {
+//   const htmlBody = document.querySelector("body") as HTMLBodyElement;
+//   htmlBody.innerHTML =
+//     "<header><h1>Breakout Game</h1></header><main class='main-grid'><div class='grid-header'><button class='nameBtn'>Enter name</button><button class='playBtn'>Play Game</button><button class='helpBtn'>Help</button></div><div id='main' class='grid-gamefield'><div id='display'><div id='current-score'></div><div id='player-info'>Press play!</div></div><div id='playfield'><canvas id='playfield-canvas' width='1000' height='600'></canvas><img id='game-background' src='./src/images/background.png'></img></div></div><div class='highscore'><h2>Your Highscores:</h2></div></main>";
+// });
