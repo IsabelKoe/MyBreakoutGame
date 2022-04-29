a3898b18171bdc28a6bda2bbbda73ee3669d666a
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-29 11:15:35 +0200

    Implemented localstorage to save player name.

--- src/Player.ts
+++ src/Player.ts
@@ -1,28 +1,51 @@
 import { playerName } from "./domutils";
 
+// Hier muss noch eine Logik für implementiert werden!
 enum status {
-    BEGINNER = 'Beginner',
-    INTERMEDIATE = 'Intermediate',
-    EXPERT = 'Expert'
+  BEGINNER = "Beginner",
+  INTERMEDIATE = "Intermediate",
+  EXPERT = "Expert",
 }
 
 export interface Player {
+  playerId: number;
   name: string;
-  status?: status
+  status?: status;
+  highscore?: number;
 }
 
-export let currentPlayer: Player = {name: ''};
+let index = 0;
+let userInput: string;
+export let currentPlayer: Player = { playerId: index, name: "" };
+let playerArray: [Player] = [currentPlayer];
 
 export function askForName() {
-  let userInput = prompt("Please enter your name for the game.");
+  userInput = prompt("Please enter your name for the game.") as string;
   if (userInput != null) {
     currentPlayer = {
+      playerId: index,
       name: userInput,
     };
+    // check whether the name has been used already
+    checkPlayerName();
+    playerArray.push(currentPlayer);
   } else {
     currentPlayer = {
+      playerId: 99,
       name: "Mystery Player",
     };
-  };
-  playerName.innerHTML=`<p class="player-name">Current Player: ${currentPlayer.name} </p>`
+    playerArray.push(currentPlayer);
+  }
+  playerName.innerHTML = `<p class="player-name">Current Player: ${currentPlayer.name} </p>`;
+  localStorage.setItem("player", JSON.stringify(playerArray));
+}
+
+// check if userinput name already exist in our playerArray
+function checkPlayerName() {
+  for (let player of playerArray) {
+    if (player.name === userInput) {
+      console.log("Dein Name wurde bereits gespeichert.");
+      currentPlayer = player;
+    }
+  } 
 }
